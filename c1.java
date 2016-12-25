dbe6b01483c4936e7ace5070c788975dbf9618d4
jingliming <lmjingsd@163.com>
2016-12-25 19:11:43 +0800

    first commit

---
+++ c1.java
@@ -0,0 +1,7 @@
+package test;
+
+public class c1 {
+	public static void print(){
+		System.out.println("jinglimng");
+	}
+}
---
+++ myclass.java
@@ -0,0 +1,11 @@
+package test;
+
+public class myclass {
+	public static void main(String[] args){
+		System.out.println("我的第一个java程序");
+		c1.print();
+	}
+
+}
+
+
