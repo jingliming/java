9bf9c82d3472a0e114627390a84354df6392308d
jingliming <lmjingsd@163.com>
2017-01-02 19:52:46 +0800

    instance

--- myclass.java
+++ myclass.java
@@ -1,9 +1,35 @@
 package test;
 
 public class myclass {
+	//静态数据快
+	static {
+		System.out.println("A");
+	}
+	
+	//势力数据块
+	{
+		
+		System.out.println("1");
+	}
+	
+	{
+		
+		System.out.println("12");
+	}
+	
+	{
+		
+		System.out.println("3");
+	}
+	//构造方法
+	myclass()
+	{
+		System.out.println("myclass");
+	}
 	public static void main(String[] args){
-		System.out.println("我的第一个java程序");
-		c1.print();
+		//System.out.println("我的第一个java程序");
+		//c1.print();
+		new myclass();
 	}
 
 }
